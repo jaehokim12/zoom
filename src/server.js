0a3a296a4043d785b24e15bec30e37f0bc31ffb5
jaehokim12 <s2heart1@naver.com>
2021-11-04 19:05:01 +0900

    add send message

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
-import express from 'express';
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
 
 const app = express();
 
-console.log('hello');
-app.set('view engine', 'pug');
-app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
-
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+wss.on("connection", (socket)=>{
+    console.log("connected to browser")
+    socket.on("close", ()=>console.log("disconnected"))
+    
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'))
+        })
+    
+    socket.send('hello')
+    //front 로 메세지를 보냄
+});
+
+server.listen(3000, handleListen);
